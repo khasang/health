0cef7feb5bbc3eccc201608cae333776b1911577
skuznet <azon.sk@gmail.com>
2019-11-10 21:13:20 +0300

    fix conflict

--- src/main/java/org/health/config/AppConfig.java
+++ src/main/java/org/health/config/AppConfig.java
@@ -1,11 +1,26 @@
 package org.health.config;
 
 import org.health.dao.EmployeeDao;
+import org.health.dao.ExaminationDao;
 import org.health.dao.HorseDao;
+import org.health.dao.InspectionDao;
+import org.health.dao.ResultExaminationDao;
+import org.health.dao.RoleDao;
+import org.health.dao.UserDao;
 import org.health.dao.impl.EmployeeDaoImpl;
+import org.health.dao.impl.ExaminationDaoImpl;
 import org.health.dao.impl.HorseDaoImpl;
+import org.health.dao.impl.InspectionDaoImpl;
+import org.health.dao.impl.ResultExaminationDaoImpl;
+import org.health.dao.impl.RoleDaoImpl;
+import org.health.dao.impl.UserDaoImpl;
 import org.health.entity.Employee;
+import org.health.entity.Examination;
 import org.health.entity.Horse;
+import org.health.entity.Inspection;
+import org.health.entity.ResultExamination;
+import org.health.entity.Role;
+import org.health.entity.User;
 import org.health.model.Dog;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
