7a709c496fa59da3eafff876e01b3ab78c89807e
Evgeniy Panov <e.panov@fundcount.com>
2019-11-11 17:11:05 +0300

    002 - added swagger annotations to user

--- src/main/java/org/health/entity/User.java
+++ src/main/java/org/health/entity/User.java
@@ -2,21 +2,29 @@ package org.health.entity;
 
 import javax.persistence.*;
 
+import io.swagger.annotations.*;
+
+@ApiModel(value = "User", description = "Class representing the user.")
 @Entity
 @Table(name = "users")
 public class User {
+    @ApiModelProperty(notes = "Unique identifier", required = true, example = "1", position = 1)
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @ApiModelProperty(notes = "First name", required = true, example = "Ivan", position = 2)
     @Column(name = "first_name")
     private String firstName;
 
+    @ApiModelProperty(notes = "Last name", required = true, example = "Ivanov", position = 3)
     @Column(name = "last_name")
     private String lastName;
 
+    @ApiModelProperty(notes = "Patronymic", required = true, example = "Ivanovich", position = 4)
     private String patronymic;
 
+    @ApiModelProperty(notes = "Login", required = true, example = "Ivan123", position = 5)
     private String login;
 
     private String password;
